3e28f1e455f97f07eed754b1cece371aab59f85f
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2024-07-28 04:02:17 -0300

    Delete library-system directory

--- library-system/models/book.py
+++
@@ -1,7 +0,0 @@
-class Book:
-    def __init__(self, id: int, title: str, author: str, category: str):
-        self.id = id
-        self.title = title
-        self.author = author
-        self.category = category
-        self.is_available = True
